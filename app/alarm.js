4ea1d4d755bd2d800ffaeb06084a60ea9938d635
Lucy Mulligan <lucymulligan@hotmail.com>
2019-04-02 21:09:29 +0100

    Week 1 Progress

--- app/alarm.js
+++ app/alarm.js
@@ -6,22 +6,45 @@ const peopleThatHaveUsedAlarm = [
   { name: "Lisa", wakeUpTime: 6 }
 ];
 
-function wakeup(currentTime) {
+function GetUserNames (FirstName) { 
+  let AlarmNames = []; 
+  peopleThatHaveUsedAlarm.forEach(function (FirstName) {
+  AlarmNames.push(FirstName.name) 
+  return AlarmNames;  
+  }
+
+
+function wakeup (currentTime) {
   // currentTime will be a Javascript Date object
   // https://www.w3schools.com/jsref/jsref_obj_date.asp
   const currentHour = currentTime.getHours();
 
-  if (currentHour > 12) {
-    return "Ugggggh its too early - I'm not waking up yet";
-  } else {
-    return "Get up!! You're wasting the day";
+  switch(currentHour)  {
+    case 1:
+    case 2:
+    case 3:
+    case 4: 
+    case 5: 
+    case 6: 
+    case 7: 
+    case 8:
+    case 9:
+    case 10: 
+    case 11:  
+    return ("Ugggggh its too early - I'm not waking up yet");
+    case 12:
+    case 13:
+    case 14:
+    return ("Yesssssssssss it's lunch time!"); 
+    default:
+    return ("Get up!! You're wasting the day"); 
   }
 }
 
 function snooze() {
   let snoozeAttempts = 0;
 
-  for (let i = 0; i < 10; i++) {
+  while (snoozeAttempts < 20) {
     snoozeAttempts++;
   }
 
@@ -30,5 +53,8 @@ function snooze() {
 
 module.exports = {
   wakeup,
-  snooze
-};
+  snooze,
+  GetUserNames,
+}
+
+
--- test/alarm.test.js
+++ test/alarm.test.js
@@ -2,10 +2,22 @@ const alarm = require('../app/alarm');
 
 test('Moans if too early', () => {
   let timeNow = new Date();
-  timeNow.setHours(11);
+  timeNow.setHours(10);
   expect(alarm.wakeup(timeNow)).toBe("Ugggggh its too early - I'm not waking up yet");
 });
 
+test('Lunch time', () => {
+  let timeNow = new Date();
+  timeNow.setHours(12, 13, 14);
+  expect(alarm.wakeup(timeNow)).toBe("Yesssssssssss it's lunch time!");
+});
+
+test('Afternoon', () => {
+  let timeNow = new Date();
+  timeNow.setHours(15);
+  expect(alarm.wakeup(timeNow)).toBe("Get up!! You're wasting the day");
+});
+
 test('Snoozing for a set amount', () => {
-  expect(alarm.snooze()).toBe(10);
+  expect(alarm.snooze()).toBe(20);
 });
